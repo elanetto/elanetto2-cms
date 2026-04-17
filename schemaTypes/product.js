05d2585d1ad2c041c5548e1173bdb7f56a5793c1
elanetto <101054274+elanetto@users.noreply.github.com>
2026-04-17 19:04:28 +0200

    Lagt inn mulighet for både norsk og engelsk beskrivelse, samt størrelse og nøkkelord til produkter

--- schemaTypes/product.js
+++ schemaTypes/product.js
@@ -1,21 +1,34 @@
 export default {
   name: 'product',
-  title: 'Product',
+  title: 'Produkt',
   type: 'document',
   fields: [
     {
       name: 'title',
-      title: 'Title',
-      type: 'string',
+      title: 'Tittel',
+      type: 'object',
+      fields: [
+        {name: 'no', title: 'Norsk', type: 'string'},
+        {name: 'en', title: 'Engelsk', type: 'string'},
+      ],
+    },
+    {
+      name: 'slug',
+      title: 'URL-slug',
+      type: 'slug',
+      options: {
+        source: 'title.no',
+        maxLength: 96,
+      },
     },
     {
       name: 'price',
-      title: 'Price',
+      title: 'Pris (NOK)',
       type: 'number',
     },
     {
       name: 'image',
-      title: 'Image',
+      title: 'Bilde',
       type: 'image',
       options: {
         hotspot: true,
@@ -23,24 +36,58 @@ export default {
     },
     {
       name: 'description',
-      title: 'Description',
-      type: 'text',
+      title: 'Beskrivelse',
+      type: 'object',
+      fields: [
+        {name: 'no', title: 'Norsk', type: 'text'},
+        {name: 'en', title: 'Engelsk', type: 'text'},
+      ],
     },
     {
       name: 'inStock',
-      title: 'In Stock',
+      title: 'På lager',
       type: 'boolean',
     },
     {
       name: 'category',
-      title: 'Category',
+      title: 'Kategori',
       type: 'string',
       options: {
         list: [
-          {title: 'Sticker', value: 'sticker'},
-          {title: 'Bookmark', value: 'bookmark'},
-          {title: 'Card', value: 'card'},
+          {title: 'Klistremerke', value: 'sticker'},
+          {title: 'Bokmerke', value: 'bookmark'},
+          {title: 'Kort', value: 'card'},
         ],
+      },
+    },
+
+    // STØRRELSE
+    {
+      name: 'size',
+      title: 'Størrelse (cm)',
+      type: 'object',
+      fields: [
+        {
+          name: 'width',
+          title: 'Bredde (cm)',
+          type: 'number',
+        },
+        {
+          name: 'height',
+          title: 'Høyde (cm)',
+          type: 'number',
+        },
+      ],
+    },
+
+    // NØKKELORD
+    {
+      name: 'tags',
+      title: 'Nøkkelord',
+      type: 'array',
+      of: [{type: 'string'}],
+      options: {
+        layout: 'tags',
       },
     },
   ],
