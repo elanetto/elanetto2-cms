ce28d062a7a93b9bae4d181ee34c1dcb1fbaddd3
elanetto <101054274+elanetto@users.noreply.github.com>
2026-05-02 20:37:39 +0200

    La til muligheten for å legge til bilder i review - Sanity

--- schemaTypes/review.js
+++ schemaTypes/review.js
@@ -6,46 +6,36 @@ export default {
     {
       name: "product",
       type: "reference",
-      title: "Produkt",
       to: [{ type: "product" }],
-      validation: (Rule) => Rule.required(),
     },
     {
       name: "name",
       type: "string",
-      title: "Navn",
-      validation: (Rule) => Rule.required().min(2),
     },
     {
       name: "rating",
       type: "number",
-      title: "Rating",
-      validation: (Rule) => Rule.required().min(1).max(5),
+      validation: (Rule) => Rule.min(1).max(5),
     },
     {
       name: "comment",
       type: "text",
-      title: "Kommentar",
-      validation: (Rule) => Rule.required().min(5),
+    },
+    {
+      name: "images",
+      type: "array",
+      title: "Bilder",
+      of: [
+        {
+          type: "image",
+          options: { hotspot: true },
+        },
+      ],
     },
     {
       name: "approved",
       type: "boolean",
-      title: "Godkjent",
       initialValue: false,
     },
   ],
-
-  preview: {
-    select: {
-      title: "name",
-      subtitle: "comment",
-    },
-    prepare({ title, subtitle }) {
-      return {
-        title: `${title}`,
-        subtitle: subtitle?.slice(0, 50),
-      };
-    },
-  },
 };
